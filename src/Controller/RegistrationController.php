249651aca5d20fa9983b69bf9492348aef5f95b3
shawgoon <alban.havard@aliceadsl.fr>
2022-10-25 15:12:48 +0200

    redimension, rename avec id et supprimer originale

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -14,6 +14,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
+use Gumlet\ImageResize;
 
 class RegistrationController extends AbstractController
 {
@@ -32,6 +33,7 @@ class RegistrationController extends AbstractController
 
                 // comment obtenir l'url du dossier public ?
                 $dirUpload = str_replace("\\","/",$this->getParameter('upload_directory'))."/";
+                $dirAvatar = str_replace("\\","/",$this->getParameter('avatar_directory'))."/";
 
                 // utiliser la fonction moveUploadFile de php
                 move_uploaded_file($_FILES['registration_form']['tmp_name']['avatar'],
@@ -43,11 +45,22 @@ class RegistrationController extends AbstractController
                         $user,
                         $form->get('plainPassword')->getData()
                         )
-                );
-                
-                $entityManager->persist($user);
-                $entityManager->flush();
-                // do anything else you need here, like send an email
+                    );
+                    
+                    // je redimensionne mon image, changer son extention et je lui intègre l'id d l'user
+                    $entityManager->persist($user);
+                    $entityManager->flush();
+                    $image = new ImageResize($dirUpload.$_FILES['registration_form']['name']['avatar']);
+                    $image->resizeToWidth(100);
+                    $image->save($dirAvatar.$user->getId().'.webp', IMAGETYPE_WEBP);
+                    // pour une autre miniature je recrée une 2è image :
+                    $image2 = new ImageResize($dirUpload.$_FILES['registration_form']['name']['avatar']);
+                    $image2->resizeToWidth(256);
+                    $image2->save($dirAvatar.$user->getId().'x256.webp', IMAGETYPE_WEBP);
+                    // après mes opérations, je supprime l'image originale
+                    unlink($dirUpload.$_FILES['registration_form']['name']['avatar']);
+
+                    // do anything else you need here, like send an email
                 
                 return $userAuthenticator->authenticateUser(
                     $user,
